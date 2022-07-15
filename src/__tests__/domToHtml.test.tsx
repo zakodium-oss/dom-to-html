6090e66e5160e77f9f4e29add671333f06344a38
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-07-15 16:53:03 +0100

    test: add simple playwright test

--- src/__tests__/domToHtml.test.tsx
+++ src/__tests__/domToHtml.test.tsx
@@ -3,6 +3,11 @@ import { test, expect } from '@playwright/experimental-ct-react';
 import domToHtml from '..';
 
 test.describe('test domToHtml', () => {
+  test('basic test', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+    const title = page.locator('.navbar__inner .navbar__title');
+    await expect(title).toHaveText('Playwright');
+  });
   test.skip('default', async ({ mount }) => {
     const component = await mount(
       <div>
@@ -12,12 +17,5 @@ test.describe('test domToHtml', () => {
     const dom = await component.evaluate((dom) => dom);
     const result = await domToHtml(dom);
     expect(result).toBe('<div><span>test</span></div>');
-  });
-  test.skip('basic test', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
-    const title = page.locator('.navbar__inner .navbar__title');
-    const dom = await title.evaluate((dom) => dom);
-    const result = await domToHtml(dom);
-    expect(result).toBe('<span>Playwright</span>');
   });
 });
