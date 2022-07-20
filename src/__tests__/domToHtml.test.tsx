338299575c5a5dd85735a68824625695560d57e7
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-07-20 11:20:05 +0100

    test: add canvas and svg tests

--- src/__tests__/domToHtml.test.tsx
+++ src/__tests__/domToHtml.test.tsx
@@ -1,18 +1,52 @@
 import { test, expect } from '@playwright/experimental-ct-react';
 
-import { TestComponent } from './utils';
+import { TestComponent, CanvasTest } from './utils';
 
 test.describe('test domToHtml', () => {
-  test('simple test', async ({ mount }) => {
+  test('basic', async ({ mount }) => {
     const component = await mount(
       <TestComponent>
         <div>
-          <span>test</span>
+          <span>text</span>
         </div>
       </TestComponent>,
     );
+    const test = component.locator('#test');
+    const result = component.locator('#result');
+    expect((await test.screenshot()).buffer).toStrictEqual(
+      (await result.screenshot()).buffer,
+    );
+
     const html = component.locator('#html');
-    const result = await html.inputValue();
-    expect(result).toBe('<div><span>test</span></div>');
+    const resultHtml = await html.inputValue();
+    expect(resultHtml).toBe('<div><span>text</span></div>');
+  });
+  test.skip('svg', async ({ mount }) => {
+    const component = await mount(
+      <TestComponent>
+        <svg>
+          <circle cx="50" cy="50" r="40" />
+        </svg>
+      </TestComponent>,
+    );
+
+    const result = component.locator('#result');
+    const html = component.locator('#html');
+    const resultHtml = await html.inputValue();
+    expect(resultHtml).toBe(await result.evaluate((node) => node.innerHTML));
+  });
+  test('canvas', async ({ mount }) => {
+    const component = await mount(<CanvasTest />);
+
+    // const test = component.locator('#test');
+    const result = component.locator('#result');
+
+    // expect((await test.screenshot()).buffer).toStrictEqual(
+    //   (await result.screenshot()).buffer,
+    // );
+
+    const html = component.locator('#html');
+    const resultHtml = await html.inputValue();
+    expect(resultHtml).toBe(await result.evaluate((node) => node.innerHTML));
   });
 });
--- src/__tests__/utils.tsx
+++ src/__tests__/utils.tsx
@@ -21,7 +21,36 @@ export function TestComponent({ children }: TestComponentProps) {
       <div ref={ref} id="test">
         {children}
       </div>
+      {html && (
+        <div dangerouslySetInnerHTML={{ __html: html }} id="result"></div>
+      )}
       {html && <textarea rows={20} value={html} id="html"></textarea>}
     </div>
+  );
+}
+
+export function CanvasTest() {
+  const ref = useRef<HTMLCanvasElement>(null);
+  function initializeCanvas() {
+    const ctx = ref?.current?.getContext('2d');
+    if (ctx) {
+      ctx.fillStyle = 'rgb(200, 0, 0)';
+      ctx.fillRect(10, 10, 50, 50);
+      ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+      ctx.fillRect(30, 30, 50, 50);
+    }
+  }
+  useEffect(() => {
+    initializeCanvas();
+  }, []);
+  return (
+    <TestComponent>
+      <canvas
+        ref={ref}
+        width="200"
+        height="100"
+        style={{ border: '1px solid #000000' }}
+      />
+    </TestComponent>
   );
 }
